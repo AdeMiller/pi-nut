dce82ca7aff1feadef21a85c56642d66691229a0
Ade Miller <amiller@istreamplanet.com>
2021-01-29 21:44:13 -0800

    Add validation tasks

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e 
 
+echo -e "\e[32mInstall packages ...\e[39m"
 ansible-galaxy collection install community.general
-ansible-playbook site.yaml
+
+echo -e "\n\e[32mRun playbook ...\e[39m"
+ansible-playbook site.yaml $@
