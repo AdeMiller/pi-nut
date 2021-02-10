564a2ed892cc16f179f183b3eeb5034850bd3d60
Ade Miller <amiller@istreamplanet.com>
2021-02-09 21:06:34 -0800

    Add run command line parsing

--- run.sh
+++ run.sh
@@ -2,8 +2,28 @@
 
 set -e 
 
-echo -e "\e[32mInstall packages ...\e[39m"
+ansible_args=""
+while getopts ":uv" arg; do
+  case $arg in
+    u)
+      echo -e "\n\e[32mPi upgrade\e[39m"
+      ansible_args="--skip-tags pi_upgrade"
+      shift
+      ;;
+    v)
+      echo -e "\n\e[32mValidation mode\e[39m"
+      ansible_args="--tags validate"
+      shift
+      ;;
+    *)
+      # Pass other playbook options on.
+      ;;
+  esac
+done
+ansible_args="${ansible_args} $@"
+
+echo -e "\n\e[32mInstall packages ...\e[39m"
 ansible-galaxy collection install community.general
 
 echo -e "\n\e[32mRun playbook ...\e[39m"
-ansible-playbook site.yaml $@
+ansible-playbook site.yaml ${ansible_args}
