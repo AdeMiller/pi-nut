32c45672a5763d365abe8838f95cd13d87452a01
Ade Miller <amiller@istreamplanet.com>
2021-03-24 21:13:33 -0700

    Add support for web only run.sh

--- run.sh
+++ run.sh
@@ -2,17 +2,23 @@
 
 set -e 
 
-ansible_args=""
-while getopts ":uv" arg; do
+ansible_args="--skip-tags pi_upgrade"
+
+while getopts ":uvw" arg; do
   case $arg in
     u)
       echo -e "\n\e[32mPi upgrade\e[39m"
-      ansible_args="--skip-tags pi_upgrade"
+      ansible_args=""
       shift
       ;;
     v)
       echo -e "\n\e[32mValidation mode\e[39m"
-      ansible_args="--tags validate"
+      ansible_args="${ansible_args} --tags validate"
+      shift
+      ;;
+    w)
+      echo -e "\n\e[32mValidation mode\e[39m"
+      ansible_args="${ansible_args} --tags nut_web"
       shift
       ;;
     *)
